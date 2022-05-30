8dbd96352d89e266f10f6e129a52b4277fb9c48c
Ali Napso <napsoali@gmail.com>
2022-05-30 22:11:42 +0300

    fghgh

--- frontend/pages/admin/index.tsx
+++ frontend/pages/admin/index.tsx
@@ -6,6 +6,8 @@ import useSWR from "swr";
 import Router from "next/router";
 import Form from "components/Form";
 import { AdminNav } from "./consts";
+import LineChart from "components/indexCharts/totalDonationsByMonth";
+import { Col, Card, Row } from "react-bootstrap";
 // Make sure to check https://nextjs.org/docs/basic-features/layouts for more info on how to use layouts
 export default function SgProfile() {
 	const { loading, loggedOut, user, mutate } = useUser();
@@ -27,5 +29,51 @@ export default function SgProfile() {
 	else if (loggedOut) {
 		Router.push("/");
 	}
-	return <Layout items={AdminNav}>lol</Layout>;
+	return (
+		<Layout items={AdminNav}>
+			<Row>
+				<Col md={4}>
+					<Card bg="primary" className=" text-white mb-3 h-100">
+						<Card.Body>
+							<Card.Title as="h4" className="text-white text-center">
+								New Patrons Registered
+							</Card.Title>
+							<div className="d-flex justify-content-center">
+								<h1 className=" text-white">60</h1>
+							</div>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col md={4}>
+					<Card bg="warning" className=" text-white mb-3 h-100">
+						<Card.Body>
+							<Card.Title as="h4" className="text-white text-center ">
+								New kids Sponsored
+							</Card.Title>
+							<div className="d-flex justify-content-center">
+								<h1 className=" text-white">60</h1>
+							</div>
+						</Card.Body>
+					</Card>
+				</Col>
+				<Col md={4}>
+					<Card className="text-white bg-success  mb-3 h-100">
+						<Card.Body>
+							<Card.Title as="h4" className="text-white text-center">
+								total donations this month
+							</Card.Title>
+							<div className="d-flex justify-content-center">
+								<h1 className=" text-white">600,152</h1>
+							</div>
+						</Card.Body>
+					</Card>
+				</Col>
+			</Row>
+			<Row className="mt-4">
+				<Col>
+					<LineChart></LineChart>
+				</Col>
+			</Row>
+		</Layout>
+	);
 }
