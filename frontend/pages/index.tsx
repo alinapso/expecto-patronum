d812fa5dd1e99f0d752ab80f86ce84fc9db4a468
Ali Napso <napsoali@gmail.com>
2022-06-10 16:45:26 +0300

    Update index.tsx

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,5 +2,10 @@ import Layout, { SecurityLevel } from "components/Layout";
 import Image from "next/image";
 
 export default function Home() {
-	return <Layout securityLevel={SecurityLevel.ANONYMOUS}>Hi</Layout>;
+	return (
+		<Layout securityLevel={SecurityLevel.ANONYMOUS}>
+			<p>Hi this is the front page for expexto parunum, a non profit helping orphned childrein to find a better life</p>
+			<p>if you would like to help a chiled in need, please register and sponsor one</p>
+		</Layout>
+	);
 }
